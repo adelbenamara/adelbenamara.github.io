a60195b2fbc135e8b079fceb350ae4c14825488f
adel benamara <122398298+adelbenamara@users.noreply.github.com>
2023-02-11 23:16:29 +0100

    first

---
+++ cv.js
@@ -0,0 +1,35 @@
+ 
+
+
+
+//code js
+
+
+
+// fonction modifier le contenie de la phrase dans le header apre le click sur un boton spisifique
+  function experience(){
+
+var ph = document.getElementById('ph');
+
+
+ph.textContent="  Mon Expérience qui  affiche ici (Work Experience) si vous voulez allez a la page principale click sur Ma photo ";
+ph.style.backgroundColor="#9dfff0";
+ph.style.borderRadius="7px";
+};
+
+
+// fonction modifier le contenie de la phrase dans le header apre le click sur un boton spisifique
+ function com(){
+var ph = document.getElementById('ph');
+ph.textContent="Mes Compétences qui affiche ici  si vous voulez allez a la page principale click sur Ma photo";
+ph.style.backgroundColor="#b7ff9d";
+ph.style.borderRadius="7px";
+};
+
+// fonction modifier le contenie de la phrase dans le header apre le click sur un boton spisifique
+ function fom(){
+var ph = document.getElementById('ph');
+ph.textContent="Ma formation qui affiche ici (Education) si vous voulez allez a la page principale click sur Ma photo";
+ph.style.backgroundColor="#f9d74d";
+ph.style.borderRadius="7px";
+};
